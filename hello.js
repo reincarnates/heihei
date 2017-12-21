08d9273662b40a1136e8762583a6bd9451899541
reincarnates <2461905877@qq.com>
2017-12-21 20:26:54 +0800

    作业

---
+++ hello.js
@@ -0,0 +1,42 @@
+// console.log('我的第一个node应用')
+const xhr = 'Hello World';
+
+
+//const str = xhr.charCodeAt(1);//返回指定位置字符的Unicode编码
+//console.log(str)
+
+//const str = xhr.charAt(2);//返回指定索引位置处的字符
+//console.log(str)
+
+//const str = xhr.slice(2,4);//参数一:下标、参数二结束的下标
+//console.log(str)
+
+//const str = xhr.substring(1,3);//参数一:字符串的起始位置、参数二:字符串的结束位置(不包含)
+//console.log(str)
+
+//const str = xhr.substr(2,4);//参数一:字符串的起始位置、参数二:字符串的结束位置
+//console.log(str)
+
+//const str = xhr.indexOf(1,4);//查找某个字符在字符串中的位置。如果没有找到子字符串，则返回-1。 
+//console.log(str)
+
+//const str = xhr.lastIndexOf(1,4);//从后往前查找，返回的下标是正数的。如果没有找到子字符串，则返回-1。
+//console.log(str)
+
+//const str = xhr.toUpperCase();//转换大写
+//console.log(str)
+
+//const str = xhr.toLowerCase();//转换小写
+//console.log(str)
+
+//const str = xhr.search();//返回与正则表达式查找内容匹配的第一个字符串的位置。
+//console.log(str) 
+
+//const str = xhr.split();//字符串转数组
+//console.log(str)
+
+//const str = xhr.concat(); //字符串拼接
+//console.log(str)
+
+//const str = xhr.localeCompare();//比目标字符小返回-1、比目标字符大返回1、和目标字符串相等返回0
+//console.log(str)
